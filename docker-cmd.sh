f3af1ffc7b757d137f6443ca8b703e106d4c7b02
Indra Sigicharla <72186475+IndraSigicharla@users.noreply.github.com>
2025-06-16 11:57:52 -0400

    Update docker-cmd.sh

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -23,12 +23,10 @@ fi
 
 nginx
 
-if [ "$1" = "--debug" ]; then
-  exec su-exec "$USER" python manage.py runserver "0.0.0.0:${DJANGO_DEV_SERVER_PORT:-8000}"
-else
-  exec su-exec "$USER" gunicorn "${PROJECT_NAME}.wsgi:application" \
-       --bind "0.0.0.0:${GUNICORN_PORT:-8000}" \
-       --workers "${GUNICORN_WORKERS:-3}" \
-       --timeout "${GUNICORN_TIMEOUT:-30}" \
-       --log-level "${GUNICORN_LOG_LEVEL:-info}"
-fi
+PORT="${PORT:-8000}"
+
+exec su-exec "$USER" gunicorn "$PROJECT_NAME.wsgi:application" \
+  --bind "0.0.0.0:${PORT}" \
+  --workers  "$GUNICORN_WORKERS" \
+  --timeout  "$GUNICORN_TIMEOUT" \
+  --log-level "$GUNICORN_LOG_LEVEL"
